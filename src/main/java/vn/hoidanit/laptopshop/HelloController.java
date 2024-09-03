95378c9c0e8db70604c79799104632db8486e890
hadinhmanh <hadinhmanh2003@gmail.com>
2024-09-03 15:55:46 +0700

    bai42

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot";
+        return "Hello World Mạnh đz";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 
 }
