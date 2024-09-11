802d63bcd775cd092af37da9a2c7901ba3d83c5b
hadinhmanh <hadinhmanh2003@gmail.com>
2024-09-11 22:41:56 +0700

    bai72

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,14 +1,20 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
-import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -19,24 +25,81 @@ public class UserController {
         this.userService = userService;
     }
 
+    // Home
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("manh", test);
+        model.addAttribute("manh", "Tôi là Hà Đình Mạnh");
         return "hello";
     }
 
+    // Bảng thông tin user
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        List<User> arrUsers = this.userService.getAllUsers();
+        System.out.println(arrUsers);
+        model.addAttribute("arrUsers", arrUsers);
+        return "admin/user/Table-User";
+    }
+
+    // Thông tin 1 user
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "admin/user/User-Detail";
+    }
+
+    // Thêm mới user view
+    @RequestMapping("/admin/user/create")
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/Create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    // Xử lý thêm mới 1 user
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User manh) {
         System.out.println("Run here" + manh);
         this.userService.handleSaveUser(manh);
-        return "hello";
+        return "redirect:/admin/user";
+    }
+
+    // Update user view
+    @RequestMapping("/admin/user/update/{id}")
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("newUser", user);
+        return "admin/user/Update";
+    }
+
+    // Handle update user
+    @PostMapping("/admin/user/update")
+    public String postUpdateUser(Model model, @ModelAttribute("newUser") User manh) {
+        User currentUser = this.userService.getUserById(manh.getId());
+        if (currentUser != null) {
+            currentUser.setAddress(manh.getAddress());
+            currentUser.setFullName(manh.getFullName());
+            currentUser.setPhone(manh.getPhone());
+
+            this.userService.handleSaveUser(currentUser);
+        }
+        return "redirect:/admin/user";
+    }
+
+    // Delete user view
+    @GetMapping("/admin/user/delete/{id}")
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        model.addAttribute("newUser", new User());
+        return "admin/user/Delete";
+    }
+
+    @PostMapping("/admin/user/delete")
+    public String postDeleteUser(Model model, @ModelAttribute("newUser") User manh) {
+        User currentUser = this.userService.getUserById(manh.getId());
+        this.userService.deleteUserById(manh.getId());
+        return "redirect:/admin/user";
     }
 
 }
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.User;
@@ -13,11 +15,23 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public String handleHello() {
-        return "Hello from  service";
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUsersByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
     }
 
     public User handleSaveUser(User user) {
         return this.userRepository.save(user);
+    }
+
+    public void deleteUserById(long id) {
+        this.userRepository.deleteById(id);
     }
 }
